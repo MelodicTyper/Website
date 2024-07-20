d5ed3faa8ea445ccce0693b92be671cc26964df6
Acoustic Typer <acoustictyper@gmail.com>
2024-07-20 12:37:12 -0700

    Initialize project, basic icons and logo.

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,2 @@
+<h1>Acoustic Typer</h1>
+<img src="/AcousticTyper.svg" width="500" />
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<slot />
+
+<style>
+    /* CSS RESET */
+    
+    :global(*) {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+    }
+    :root {
+        --color-main: #66976F;
+        --color-secondary: #8ff0a4;
+    }
+    
+</style>
